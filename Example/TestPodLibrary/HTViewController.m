dd1959a3da01b918b10c3ef2e68e1dd080833cb0
lihongtao <1004646858@qq.com>
2017-04-19 15:31:06 +0800

    Initial Commit of Library
    Signed-off-by: lihongtao <1004646858@qq.com>

--- Example/TestPodLibrary/HTViewController.m
+++ Example/TestPodLibrary/HTViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "HTViewController.h"
+#import "UIImageView+WebCache.h"
+#import "UIView+Frame.h"
 
 @interface HTViewController ()
 
@@ -17,6 +19,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UIImageView *image = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 100, 100)];
+    image.center = self.view.center;
+
+    [image sd_setImageWithURL:[NSURL URLWithString:@"http://pic.58pic.com/58pic/13/61/00/61a58PICtPr_1024.jpg"] completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+        
+    }];
+    [self.view addSubview:image];
+
+    NSLog(@"view %@",@(image.width));
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
